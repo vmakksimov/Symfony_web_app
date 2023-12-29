3d840722bc0e1cd484bd32d26d9fcb7e309a6ce3
Viktor <vmakksimov@gmail.com>
2023-12-29 19:38:53 +0200

    adding symfony mime

--- web_app/src/Controller/DefaultController.php
+++ web_app/src/Controller/DefaultController.php
@@ -1,33 +1,67 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use App\Services\GiftService;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]    
-    public function index(EntityManagerInterface $entityManager,PersistenceManagerRegistry $doctrine, GiftService $gifts) : Response
-    {
+    #[Route('/defaultf/{page?}', name: 'app_default', requirements: ['page' => '\d+'])]
 
-        $user = new User();
-        $user->setName('Rodrigo');
-       
-        $entityManager->persist($user);
-        $entityManager->flush();
-        
+
+    public function index(
+        EntityManagerInterface $entityManager,
+        PersistenceManagerRegistry $doctrine,
+        GiftService $gifts,
+        Request $request,
+        SessionInterface $session
+    ): Response {
+
+        // $user = new User();
+        // $user->setName('Rodrigo');
+
+        // $entityManager->persist($user);
+        // $entityManager->flush();
+
 
         $users = $doctrine->getManager()->getRepository(User::class)->findAll();
-        
-      
+        $session->set('name', 'session value');
+        $session->clear();
+        if ($session->has('name')) {
+            exit($session->get('name'));
+        }
+        // exit($request->cookies->get('PHPSESSID'));
+        // $cookie = new Cookie(
+        //     'bounty',
+        //     'bounty_value',
+        //     time() + (2 * 365 * 24 * 60 * 60)
+        // );
+
+        // $res = new Response();
+        // $res->headers->setCookie($cookie);
+        // $res->send();
+
+
         $usera = ['Adam', 'Philly', 'Asen'];
+
+        if (!$users) {
+            throw $this->createNotFoundException('The user does not exists');
+        }
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
             'users' => $users,
@@ -35,10 +69,18 @@ class DefaultController extends AbstractController
         ]);
         // return $this->redirectToRoute('app_default2');
     }
-    
-    #[Route('/default2', name: 'app_default2')]
+
+    #[Route('/default2/{page}/{number}/{slug}/{category}', name: 'app_default2', defaults: ['category' => 'computers'],  requirements: ['category' => 'computers|rtv'])]
     public function index2(): Response
     {
         return $this->json(['Message' => 'This is defalt user 2']);
+    }
+
+    #[Route("/download", name: "download")]
+
+    public function download()
+    {
+        $path = $this->getParameter('download_directory');
+        return $this->file($path . 'file1.png');
     }
 }
