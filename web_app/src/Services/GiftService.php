a170b3cbb4621f38c77f06121a38325231fe5c94
Viktor <vmakksimov@gmail.com>
2023-12-30 03:38:20 +0200

    adding app fixtures

--- web_app/src/Services/GiftService.php
+++ web_app/src/Services/GiftService.php
@@ -1,15 +1,18 @@
 <?php
 
 declare(strict_types=1);
+
 namespace App\Services;
+
 use Psr\Log\LoggerInterface;
 
-class GiftService {
+class GiftService
+{
 
     public $gifts = ['ball', 'feather', 'egg', 'tomato', 'rosmarin'];
-    public function __construct(LoggerInterface $logger){
+    public function __construct(LoggerInterface $logger)
+    {
         $logger->info('Succesfully presented gifts!');
         $this->gifts;
     }
 }
-
